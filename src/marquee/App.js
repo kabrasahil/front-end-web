657e2cb2f6c7c452cb650c35856240dd01a2aa3c
samyak jain <43640144+samyak1512@users.noreply.github.com>
2023-02-18 13:12:30 +0530

    Create App.js

---
+++ src/marquee/App.js
@@ -0,0 +1,12 @@
+import Announcement from "./Announcement";
+import "./styles.css";
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <Announcement/>
+    </div>
+  );
+}
+
