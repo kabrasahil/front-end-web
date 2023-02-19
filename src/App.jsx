90b38dd645c87537a8a7df7da8fecea13afbeb59
Parikshit <parikshit.p2002@gmail.com>
2023-02-20 00:42:33 +0530

    changed-look-of-home

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Navbar from "./Navbar";
 import Stile from "./home/Stile";
 import Ttile from "./home/Ttile";
 import Announcement from "./home/marquee/Announcement";
+import HomeArticle from "./home/HomeArticles";
+import FFtile from "./home/FFtile";
 
 function App() {
   return (
@@ -12,8 +14,12 @@ function App() {
       <Navbar />
       <Announcement />
       <Ftile />
+      <hr />
       <Stile />
+      <hr />
       <Ttile />
+      <hr />
+      <FFtile />
       <Footer />
     </div>
   );
