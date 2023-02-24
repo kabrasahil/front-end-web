5ee53e4d229c11295b97aabc58c779b668dac567
Parikshit <parikshit.p2002@gmail.com>
2023-02-24 21:50:27 +0530

    made some changes

---
+++ src/home/components/MeetTheTeamCarousel.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const VisibleDiv = (visible) => {
+  return <div>{visible.map((el, i) => {
+
+    
+
+  })}</div>;
+};
+
+const MeetTheTeamCarousel = () => {
+  return <div></div>;
+};
+
+export default MeetTheTeamCarousel;
