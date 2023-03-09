00c4617b00ab31af827288eb97316fa8f78467b8
Parikshit <parikshit.p2002@gmail.com>
2023-03-09 23:32:13 +0530

    made some changes

---
+++ src/article/shareButton/ShareButtons.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const ShareButtons = ({ articleUrl }) => {
+  // generate the shareable message
+
+  const sharingButtonsData = [
+    {
+      name: "facebook",
+      url: "", // generated url
+    },
+    {
+      name: "twitter",
+      url: "", // genrerated url
+    },
+  ];
+
+  return (
+    <div className="">
+      {sharingButtonsData.map((el) => {
+        return (
+          <button
+            className=""
+            onClick={(e) => {
+              window.open(el.url, "_blank");
+            }}
+          >
+            {/* icons related to the current el */}
+            {/* add url of the current el to the button */}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default ShareButtons;
