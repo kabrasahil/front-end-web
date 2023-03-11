4dc4b85d2d23d7afde007fe7d386545cebc7db3e
Keshav <keshavgarg8800@gmail.com>
2023-03-12 00:00:36 +0530

    keshav commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import Navbar from "./header/Navbar";
 import Home from "./home/Home";
 import Announcement from "./marquee/Announcement";
 
+import ShareButton from "./article/shareButton/ShareButtons";
+
+
 function App() {
   return (
     <div>
       <Navbar />
       <Announcement />
-
+      {/* <ShareButton url="https://twitter.com/" socialMedia="twitter" /> */}
       <Home />
       <Footer />
     </div>
--- src/article/shareButton/ShareButtons.jsx
+++ src/article/shareButton/ShareButtons.jsx
@@ -1,36 +1,79 @@
 import React from "react";
+import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
+import "./sharebutton.css";
+import { ShareButton, FacebookShareButton, TwitterShareButton, WhatsappShareButton, EmailShareButton, FacebookIcon, TwitterIcon, WhatsappIcon, EmailIcon } from 'react-share';
 
-const ShareButtons = ({ articleUrl }) => {
-  // generate the shareable message
+// const ShareButton = ({ url, socialMedia }) => {
+//   const handleClick = () => {
+//     switch (socialMedia) {
+//       case "twitter":
+//         window.open(
+//           `${url}`,
+//           "_blank"
+//         );
+//         break;
+//       case "facebook":
+//         window.open(
+//           `${url}`,
+//           "_blank"
+//         );
+//         break;
+//       case "instagram":
+//         window.open(
+//           `${url}`,
+//           "_blank"
+//         );
+//         break;
+//       default:
+//         break;
+//     }
+//   };
 
-  const sharingButtonsData = [
-    {
-      name: "facebook",
-      url: "", // generated url
-    },
-    {
-      name: "twitter",
-      url: "", // genrerated url
-    },
-  ];
+//   const getIcon = () => {
+//     switch (socialMedia) {
+//       case "twitter":
+//         return <FaTwitter className="icon" />;
+//       case "facebook":
+//         return <FaFacebook className="icon" />;
+//       case "instagram":
+//         return <FaInstagram className="icon" />;
+//       default:
+//         return null;
+//     }
+//   };
 
+//   return (
+//     <button onClick={handleClick}>
+//       {getIcon()}
+//       <span className="label">
+//         Share on {socialMedia.charAt(0).toUpperCase() + socialMedia.slice(1)}
+//       </span>
+//     </button>
+//   );
+// };
+
+// export default ShareButton;
+
+
+function ShareButtons({ url, title }) {
   return (
-    <div className="">
-      {sharingButtonsData.map((el) => {
-        return (
-          <button
-            className=""
-            onClick={(e) => {
-              window.open(el.url, "_blank");
-            }}
-          >
-            {/* icons related to the current el */}
-            {/* add url of the current el to the button */}
-          </button>
-        );
-      })}
+    <div>
+      <FacebookShareButton url="https://medium.com/game-theory-in-competitive-programming/sprague-grundy-theorem-5938233fb64b" quote={title}>
+        <FacebookIcon size={32} round={true} />
+      </FacebookShareButton>
+      <TwitterShareButton url={url} title={title}>
+        <TwitterIcon size={32} round={true} />
+      </TwitterShareButton>
+      <WhatsappShareButton url={url} title={title}>
+        <WhatsappIcon size={32} round={true} />
+      </WhatsappShareButton>
+      <EmailShareButton url={url} subject={title}>
+        <EmailIcon size={32} round={true} />
+      </EmailShareButton>
     </div>
   );
-};
+}
 
 export default ShareButtons;
+
+
