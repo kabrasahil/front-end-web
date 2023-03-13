13a9e3c212e507cb206d76dac9b88b04be68afaf
Parikshit <parikshit.p2002@gmail.com>
2023-03-13 15:42:41 +0530

    made some changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,13 @@ import Home from "./home/Home";
 import Announcement from "./marquee/Announcement";
 
 import ShareButton from "./article/shareButton/ShareButtons";
+import ArticleContent from "./article/articleContent/ArticleContent";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <ArticleContent />
       <Announcement />
       {/* <ShareButton url="https://twitter.com/" socialMedia="twitter" /> */}
       <Home />
