80c33e47efe3c531621e6ec86bcf7d2c2f229788
Vishal-jatia <97461410+Vishal-jatia@users.noreply.github.com>
2023-03-18 01:08:24 +0530

    Home new design components

---
+++ src/home/HomeUpdates.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export default function HomeUpdates() {
+    return(
+        <div className='my-10 bg-stone-900 box-border grid mx-80 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2'>
+            <div className='w-64 text-center'>
+                <img className='shadow-md shadow-black w-64 h-44 rounded-3xl object-cover' src='https://p19-sign.tiktokcdn-us.com/tos-useast5-avt-0068-tx/234bee5baac21e4f3da4d5265ba5aad0~c5_720x720.jpeg?x-expires=1679000400&x-signature=3z%2BQLvAFfKLglim4RmGL3jjlekM%3D' alt='' />
+                <p className='px-2 py-4 text-slate-400 text-sm'>jaisdji asd aisdjiasjd iajsdioajs diajsdi asdijasi difj idajfisjf jaisodjfoija sdjaijsdioajsd oiasjd </p>
+            </div>
+            <div className='w-64 text-center'>
+                <img className='shadow-md shadow-black w-64 h-44 rounded-3xl object-cover' src='https://p19-sign.tiktokcdn-us.com/tos-useast5-avt-0068-tx/234bee5baac21e4f3da4d5265ba5aad0~c5_720x720.jpeg?x-expires=1679000400&x-signature=3z%2BQLvAFfKLglim4RmGL3jjlekM%3D' alt='' />
+                <p className='px-2 py-4 text-slate-400 text-sm'>jaisdji asd aisdjiasjd iajsdioajs diajsdi asdijasi difj idajfisjf jaisodjfoija sdjaijsdioajsd oiasjd </p>
+            </div>
+            <div className='w-64 text-center'>
+                <img className='shadow-md shadow-black w-64 h-44 rounded-3xl object-cover' src='https://p19-sign.tiktokcdn-us.com/tos-useast5-avt-0068-tx/234bee5baac21e4f3da4d5265ba5aad0~c5_720x720.jpeg?x-expires=1679000400&x-signature=3z%2BQLvAFfKLglim4RmGL3jjlekM%3D' alt='' />
+                <p className='px-2 py-4 text-slate-400 text-sm'>jaisdji asd aisdjiasjd iajsdioajs diajsdi asdijasi difj idajfisjf jaisodjfoija sdjaijsdioajsd oiasjd </p>
+            </div>
+            <div className='w-64 text-center'>
+                <img className='shadow-md shadow-black w-64 h-44 rounded-3xl object-cover' src='https://p19-sign.tiktokcdn-us.com/tos-useast5-avt-0068-tx/234bee5baac21e4f3da4d5265ba5aad0~c5_720x720.jpeg?x-expires=1679000400&x-signature=3z%2BQLvAFfKLglim4RmGL3jjlekM%3D' alt='' />
+                <p className='px-2 py-4 text-slate-400 text-sm'>jaisdji asd aisdjiasjd iajsdioajs diajsdi asdijasi difj idajfisjf jaisodjfoija sdjaijsdioajsd oiasjd </p>
+            </div>
+            <div className='w-64 text-center'>
+                <img className='shadow-md shadow-black w-64 h-44 rounded-3xl object-cover' src='https://p19-sign.tiktokcdn-us.com/tos-useast5-avt-0068-tx/234bee5baac21e4f3da4d5265ba5aad0~c5_720x720.jpeg?x-expires=1679000400&x-signature=3z%2BQLvAFfKLglim4RmGL3jjlekM%3D' alt='' />
+                <p className='px-2 py-4 text-slate-400 text-sm'>jaisdji asd aisdjiasjd iajsdioajs diajsdi asdijasi difj idajfisjf jaisodjfoija sdjaijsdioajsd oiasjd </p>
+            </div>
+            <div className='w-64 text-center text-slate-100 flex flex-col gap-3 '>
+                <div className='w-64 h-20 bg-stone-800 hover:bg-stone-700 rounded-2xl items-center'><p className='py-6'>Blogs</p></div>
+                <div className='w-64 h-20 bg-stone-800 hover:bg-stone-700 rounded-2xl items-center'><p className='py-6'>Events</p></div>
+            </div>
+        </div>
+    )
+}
