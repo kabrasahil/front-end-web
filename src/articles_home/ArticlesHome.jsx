66d5318b9d0323497c7aa5f302f85ee981c7eb32
samyak jain <2samyakjain@gmail.com>
2023-03-19 00:19:46 +0530

    changes made for blogheading

--- src/articles_home/ArticlesHome.jsx
+++ src/articles_home/ArticlesHome.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import ArticleList from "./articleList/AtricleList";
 import RightSide from "./rightSide/RightSide";
+import BlogHeading from "./blogHeading/App";
 
 const ArticlesHome = () => {
   return (
     <div className="flex flex-col min-h-screen">
       {/* Navbar */}
       <div className="w-full bg-gray-900">Navbar content</div>
+      {/* blog heading */}
+      <div className="w-full bg-gray-900">
+        <BlogHeading />
+      </div>
 
       {/* Main content */}
       <div className="flex flex-col flex-grow container mx-auto px-4">
---
+++ src/articles_home/blogHeading/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className='bg-stone-900'>
+        {/* make a small heading showing blogs and with background colour slate-500 */}
+        <div className='text-center'>
+            <h1 class="bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text mt-20 text-4xl font-sans font-semibold">BLOGS</h1>  
+            </div>      {/* make a much larger heading showing "Releases,insights,and company announcements" and make it white in colour with text justified in center */}
+        <h1 className='text-center text-white text-5xl font-bold mt-5'>Releases, insights, and </h1>
+        <h1 className='text-center text-white text-5xl font-bold p-2'>company announcements</h1>
+
+    </div>
+  );
+}
+
+export default App;
