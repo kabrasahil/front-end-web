b1d5fd5e9d2efeaf2d133cf7bcbf8132d77f0f98
Vishal Jatia <97461410+Vishal-jatia@users.noreply.github.com>
2023-05-25 01:35:05 +0530

    Update Footer.jsx

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -67,7 +67,7 @@ const Footer = () => {
     >
       <EarthCanvas />
     </motion.div>
-        <div className="flex lg:flex-row sm:flex-col md:flex-col mt-4 gap-y-4 gap-x-4 sm:justify-center lg:text-base sm:text-xl md:text-2xl">
+        <div className="flex lg:flex-row sm:flex-row md:flex-row mt-4 gap-y-4 gap-x-4 sm:justify-center lg:text-base sm:text-xl md:text-2xl">
           <a
             href="https://www.facebook.com/igts.nsit/"
             className="text-gray-300 hover:text-gray-500 dark:hover:text-white"
